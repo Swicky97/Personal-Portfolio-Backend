44259f057c0955bf972c40581640c5ee0bcfa5d0
Chase Swick <chaseswick@gmail.com>
2024-09-23 17:26:32 -0400

    Create schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,12 @@
-import testimonials from './testimonials.js'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
 
 export const schemaTypes = 
 [
-    testimonials
+    abouts, brands, contact, experiences, skills, testimonials, workExperience
 ]
